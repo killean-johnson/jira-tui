b7a4588881b17e0ef9be10ec98103d82de6f9c5c
Killean Johnson <killean@Killeans-MacBook-Pro.local>
2021-12-30 22:21:42 -0500

    Cleanup old unused code

--- main.go
+++ main.go
@@ -17,24 +17,7 @@ func main() {
 	// Set up the client connection
 	client := &api.JiraClient{}
 	client.Connect(conf.Email, conf.APIToken, conf.JiraURL)
-	/* issueKey := "VV-392"
-	transitions, _, _ := client.GetConnection().Issue.GetTransitions(issueKey)
-	for _, tran := range transitions {
-		fmt.Printf("%#v\n", tran)
-	}
 
-	tid := ""
-	for _, t := range transitions {
-		if t.Name == "Pending / Testing" {
-			tid = t.ID
-			break
-		}
-	}
-
-	_, err = client.GetConnection().Issue.DoTransition(issueKey, tid)
-	panic(err)
-	return */
-	//client.Issue.GetTransitions(issueKey)
 	// Run the TUI
 	t := new(tui.TUI)
 	t.SetupTUI(client, conf)
@@ -42,6 +25,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	//tui.CreateTUI(client, conf)
 }
